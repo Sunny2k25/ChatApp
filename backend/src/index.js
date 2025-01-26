337aee5c4bad835c0e0046c95b3730920e64c1b3
Sunny Jaiswal <jaiswalsunny055@gmail.com>
2025-01-27 00:26:51 +0530

    Project Completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -27,13 +27,12 @@ app.use(
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-if(process.env.NODE_ENV === "production"){
-  app.use(express.static(path.join(_dirname, '../frontend/dist')));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(_dirname, "../frontend/dist")));
 
-
-  app.get("*",(req,res)=>{
+  app.get("*", (req, res) => {
     res.sendFile(path.resolve(_dirname, "..frontend", "dist", "index.html"));
-  })
+  });
 }
 
 server.listen(PORT, () => {
